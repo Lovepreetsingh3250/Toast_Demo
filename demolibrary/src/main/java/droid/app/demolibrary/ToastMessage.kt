40663786eec55766fa32298634539812133bf938
Ankush Shukla <ankush.shukla@ldh.01s.in>
2021-05-26 11:35:31 +0530

    first commit

---
+++ demolibrary/src/main/java/droid/app/demolibrary/ToastMessage.kt
@@ -0,0 +1,10 @@
+package droid.app.demolibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastMessage {
+    fun showToast(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show()
+    }
+}
