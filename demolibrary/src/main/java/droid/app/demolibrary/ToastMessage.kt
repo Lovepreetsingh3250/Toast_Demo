03342d47c79068348321cd1e25daaef3528b10a0
Ankush Shukla <ankush.shukla@ldh.01s.in>
2021-05-26 14:19:08 +0530

    internetCheck

--- demolibrary/src/main/java/droid/app/demolibrary/ToastMessage.kt
+++ demolibrary/src/main/java/droid/app/demolibrary/ToastMessage.kt
@@ -1,10 +1,21 @@
 package droid.app.demolibrary
 
 import android.content.Context
+import android.net.ConnectivityManager
 import android.widget.Toast
+
 
 class ToastMessage {
     fun showToast(c: Context?, message: String?) {
+
         Toast.makeText(c, message, Toast.LENGTH_LONG).show()
+
+    }
+
+    public fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager
+                .activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
